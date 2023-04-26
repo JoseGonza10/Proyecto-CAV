1555805f7694bde5ff7ad24604f9157db455468e
JoseGonza10 <JoseGonza10>
2023-04-26 16:32:41 -0600

    Commit changes 26/04/2023

--- src/main/java/com/proyecto/cav/controller/HtmlController.java
+++ src/main/java/com/proyecto/cav/controller/HtmlController.java
@@ -4,83 +4,87 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 @Slf4j
 public class HtmlController {
-    
+
     @GetMapping("/index")
-    public String indexViewer(){
+    public String indexViewer() {
         return "index";
     }
 
     @GetMapping("/register")
-    public String registerViewer(){
+    public String registerViewer() {
         return "register";
-    } 
-    
+    }
+
     @GetMapping("/bienvenidoAdmin")
-    public String bVAdminViewer(){
+    public String bVAdminViewer() {
         return "bienvenidoAdmin";
-    }  
-    
+    }
+
     @GetMapping("/bienvenidoEstudiante")
-    public String bVEstudianteViewer(){
+    public String bVEstudianteViewer() {
         return "bienvenidoEstudiante";
-    } 
-    
+    }
+
+    @GetMapping("/estudiante/bienvenidoEstudiante")
+    public String retornarBienvenidoEstudiante() {
+        return "/estudiante/bienvenidoEstudiante";
+    }
+
     @GetMapping("/bienvenidoProfesor")
-    public String bVProfesorViewer(){
+    public String bVProfesorViewer() {
         return "bienvenidoProfesor";
-    }  
-    
+    }
+
     @GetMapping("/certificacion")
-    public String certificacionViewer(){
+    public String certificacionViewer() {
         return "certificacion";
-    } 
-    
+    }
+
     @GetMapping("/cambioCuenta")
-    public String cambioCuentaViewer(){
+    public String cambioCuentaViewer() {
         return "cambioCuenta";
     }
-    
+
     @GetMapping("/loginEst")
-    public String loginEstViewer(){
+    public String loginEstViewer() {
         return "loginEst";
-    } 
-    
+    }
+
     @GetMapping("/loginAdm")
-    public String loginAdmViewer(){
+    public String loginAdmViewer() {
         return "loginAdm";
-    } 
-    
+    }
+
     @GetMapping("/loginProf")
-    public String loginProfViewer(){
+    public String loginProfViewer() {
         return "loginProf";
-    } 
-    
+    }
+
     @GetMapping("/extra")
-    public String promocionViewer(){
+    public String promocionViewer() {
         return "extra";
-    } 
-    
+    }
+
     @GetMapping("/areas/areaIngenierias")
-    public String areaIngenieriasViewer(){
+    public String areaIngenieriaViewer() {
         return "/areas/areaIngenierias";
     }
-    
+
+    @GetMapping("/areas/areaIt")
+    public String areaItViewer() {
+        return "/areas/areaIt";
+    }
+
     @GetMapping("/areas/areaSocial")
-    public String areaSocialViewer(){
+    public String areaSocialViewer() {
         return "/areas/areaSocial";
     }
-    
+
     @GetMapping("/areas/areaSalud")
-    public String areaSaludViewer(){
+    public String areaSaludViewer() {
         return "/areas/areaSalud";
-    }
-    
-    @GetMapping("/areas/areaIt")
-    public String areaItViewer(){
-        return "/areas/areaIt";
     }
 }
